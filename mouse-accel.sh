5657510e2f1af36af1fd5318961d68ad72cdb5b6
Oscar R Acuña <acunainfosec@proton.me>
2024-06-29 17:35:57 -0600

    Create mouse-accel.sh
    This gets rid of mouse acceleration.

---
+++ mouse-accel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''Logitech USB Receiver' 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop 'pointer:''Logitech USB Receiver' 'libinput Accel Speed' -0.1
